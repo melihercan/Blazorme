207d1ea23397c7151cae0fe72d2f22d9faeef64e
Melih Ercan <melihercan@gmail.com>
2020-05-25 14:47:12 +0300

    Added diff.js and diff2html.js packges to add diff line by line or side by side as well.

---
+++ Diff/Diff2HtmlConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Diff
+{
+
+    internal class Diff2HtmlConfiguration
+    {
+        // "line-by-line" or "side-by-side", default is "line-by-line"
+        internal const string SideBySide = "side-by-side";
+        internal const string LineByLine = "line-by-line";
+        public string outputFormat { get; set; }
+
+        // default is true
+        public bool drawFileList { get; set; }
+
+        // show differences level in each line: word or char, default is word
+        internal const string Word = "word";
+        internal const string Char = "char";
+        public string diffStyle { get; set; }
+
+        // matching level: 'lines' for matching lines, 'words' for matching lines and words or 'none', default is none
+        internal const string Lines = "lines";
+        internal const string Words = "words";
+        internal const string None = "none";
+        public string matching { get; set; } 
+    }
+}
--- Diff/DiffJsInterop.cs
+++ Diff/DiffJsInterop.cs
@@ -8,14 +8,47 @@ namespace Diff
 {
     internal class DiffJsInterop
     {
-        public static ValueTask<string> Invoke(IJSRuntime jsRuntime, string firstHtml, string secondHtml)
+        public static async ValueTask<string> Invoke(IJSRuntime jsRuntime, string firstHtml, string secondHtml)
         {
+            var stringOne = "string 1 here";
+            var stringTwo = "string 2 here";
+
+            string diffString = string.Empty;
+            diffString = await jsRuntime.InvokeAsync<string>(
+                "Diff.createTwoFilesPatch",
+                new object[]
+                {
+                    "strings", "strings", stringOne, stringTwo
+                });
+
+            Console.WriteLine($" DIFF: {diffString}");
+
+            var diffHtml = await jsRuntime.InvokeAsync<string>(
+                "Diff2Html.html",
+                new object[]
+                {
+                    diffString,
+                    new Diff2HtmlConfiguration
+                    {
+                        drawFileList = false,
+                        matching = Diff2HtmlConfiguration.Words,
+                        outputFormat = Diff2HtmlConfiguration.SideBySide
+                    }
+                });
+            Console.WriteLine($" DIFF: {diffHtml}");
+            return diffHtml;
+
             // Implemented in htmldiff.js
-            return jsRuntime.InvokeAsync<string>(
-                "htmldiff",
-                new object[] {
-                    firstHtml,
-                    secondHtml});
+            //return await jsRuntime.InvokeAsync<string>(
+            //    "htmldiff",
+            //    new object[] 
+            //    {
+            //        firstHtml,
+            //        secondHtml
+            //    });
+
+
+
         }
 
     }
