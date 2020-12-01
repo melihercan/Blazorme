6451fa0b02365fcfca348e8940da040ae054ff1d
Melih Ercan <melihercan@gmail.com>
2020-12-01 19:04:39 +0300

    Added Steve Sanderson's TestHost component.

---
+++ TestHost/TestHtmlDocument.cs
@@ -0,0 +1,14 @@
+﻿using HtmlAgilityPack;
+
+namespace BlazormeTestHost
+{
+    internal class TestHtmlDocument : HtmlDocument
+    {
+        public TestHtmlDocument(TestRenderer renderer)
+        {
+            Renderer = renderer;
+        }
+
+        public TestRenderer Renderer { get; }
+    }
+}
