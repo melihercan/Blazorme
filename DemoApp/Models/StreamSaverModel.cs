36f99a213b5ab5eb67da84d3a96c01b64f83a1a7
Melih Ercan <melihercan@gmail.com>
2021-06-06 13:36:42 +0300

    StreamSaver page ongoing implementation.

---
+++ DemoApp/Models/StreamSaverModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoApp.Models
+{
+    public class StreamSaverModel
+    {
+        public string Filename { get; set; }
+        public string Text { get; set; }
+        public string LoremIpsum { get; set; }
+    }
+}
--- DemoApp/Pages/StreamSaverDemo.razor.cs
+++ DemoApp/Pages/StreamSaverDemo.razor.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DemoApp.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,45 @@ namespace DemoApp.Pages
 {
     public partial class StreamSaverDemo
     {
+        private StreamSaverModel _streamSaverModel = new();
+
+    
+
+        private void ClearFilename()
+        {
+
+        }
+
+
+        private void AddText()
+        {
+
+        }
+
+        private void ClearText()
+        {
+
+        }
+
+        private void AddLoremIpsum()
+        {
+
+        }
+
+
+        private async Task AppendAsync()
+        {
+
+        }
+
+        private async Task Close()
+        {
+
+        }
+
+        private void Reset()
+        {
+
+        }
     }
 }
